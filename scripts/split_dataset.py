29e8d064bbb61dda3d1e80a080035c2602954632
Raffaele Terracino <raffaeleterracino001@gmail.com>
2025-06-02 18:23:35 +0200

    First commit: templates for experiments

---
+++ scripts/split_dataset.py
@@ -0,0 +1,58 @@
+import os
+import random
+import shutil
+from pathlib import Path
+
+# Per avere risultati ripetibili
+random.seed(42)
+
+#path dataset merged
+base_dir = Path(".")
+original_images = base_dir / "merged" / "images"
+original_labels = base_dir / "merged" / "labels"
+
+#divisione effettuata
+train_perc = 0.7
+val_perc= 0.2
+#test_perc = 0.1
+
+# legge tutti i nomi di file jpg
+image_list = list(original_images.glob("*.jpg"))
+
+#Mescola i file,
+random.shuffle(image_list)
+
+# calcola il numero di file per ogni split
+n_total = len(image_list)
+n_train = int(train_perc * n_total)
+n_val = int(val_perc * n_total)
+
+train_files = image_list[:n_train]
+val_files = image_list[n_train:n_train + n_val]
+test_files = image_list[n_train + n_val:]
+
+splits = {
+    "train": train_files,
+    "valid": val_files,
+    "test": test_files
+}
+
+#funzione utility per copiare immagini e relative labe
+def copy_files(file_list, split_name):
+    img_out_dir = base_dir / split_name / "images"
+    lbl_out_dir = base_dir / split_name / "labels"
+    img_out_dir.mkdir(parents=True, exist_ok=True)
+    lbl_out_dir.mkdir(parents=True, exist_ok=True)
+
+    for img_path in file_list:
+        #copia dell'immagine
+        shutil.copy2(img_path, img_out_dir / img_path.name)
+        
+        #copia della label
+        label_path = original_labels / (img_path.stem + ".txt")
+        shutil.copy2(label_path, lbl_out_dir / label_path.name)
+
+for split_name, files in splits.items():
+    copy_files(files, split_name)
+
+print("Split effettuato")
